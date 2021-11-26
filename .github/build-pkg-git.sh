c2e5530269eeaf20b08feffa35be8cf0e1322d57
Thomas Calderon <calderon.thomas@gmail.com>
2021-11-26 14:22:32 +0000

    Initial Debian package CI tools

---
+++ .github/build-pkg-git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OUTPUT=/tmp/output
+
+echo Generating package for ${BRANCH_NAME}
+COMMIT_SHORT=1.0.x-$(git rev-parse --short HEAD)
+git archive --format=tar.gz --prefix=caml-crush-$COMMIT_SHORT/ master > ../caml-crush-$COMMIT_SHORT.tar.gz
+git checkout debian
+gbp import-orig --debian-branch=debian -u $COMMIT_SHORT ../caml-crush-$COMMIT_SHORT.tar.gz
+
+COMMIT_TS=$(git show -s --pretty=format:%ct HEAD)
+COMMIT_DATE=$(date --date="@${COMMIT_TS}" +%Y%m%d%H%M)
+GBP_SINCE_PARAM="--auto"
+export DEBEMAIL="calderon.thomas@gmail.com"
+export DEBFULLNAME="Thomas Calderon"
+gbp dch $GBP_SINCE_PARAM --snapshot --snapshot-number="${COMMIT_DATE}" --no-multimaint --ignore-branch
+
+gbp buildpackage -uc -us --git-no-sign-tags --git-debian-branch=debian --git-ignore-new --git-export-dir=$OUTPUT
